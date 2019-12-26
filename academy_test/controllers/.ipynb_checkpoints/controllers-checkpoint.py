368e133a338994d084a83fa4f2085700c1556a58
ElnurShabanov <elnurshabanov@gmail.com>
2019-12-26 12:37:45 +0000

    re-test

--- academy_test/controllers/.ipynb_checkpoints/controllers-checkpoint.py
+++ academy_test/controllers/.ipynb_checkpoints/controllers-checkpoint.py
@@ -2,20 +2,18 @@
 from odoo import http
 
 
-# class AcademyTest(http.Controller):
-#     @http.route('/academy_test/academy_test/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/academy_test/academy_test/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('academy_test.listing', {
-#             'root': '/academy_test/academy_test',
-#             'objects': http.request.env['academy_test.academy_test'].search([]),
-#         })
-
-#     @http.route('/academy_test/academy_test/objects/<model("academy_test.academy_test"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('academy_test.object', {
-#             'object': obj
-#         })
+class AcademyTest(http.Controller):
+    @http.route('/academy_test/academy_test/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
+    @http.route('/academy_test/academy_test/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('academy_test.listing', {
+            'root': '/academy_test/academy_test',
+            'objects': http.request.env['academy_test.academy_test'].search([]),
+        })
+    @http.route('/academy_test/academy_test/objects/<model("academy_test.academy_test"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('academy_test.object', {
+            'object': obj
+        })
